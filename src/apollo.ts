68460aa4a3df04c92841f27c66e1cdd523f950e1
joonho319 <jejunho820@gmail.com>
2021-09-05 17:48:10 +0900

    link

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,5 @@
 import { ApolloClient, createHttpLink, InMemoryCache, makeVar, split } from '@apollo/client';
 import { setContext } from "@apollo/client/link/context";
-import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from '@apollo/client/utilities';
 import { LOCALSTORAGE_TOKEN } from './constant';
 
@@ -8,19 +7,6 @@ const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
-
-const wsLink = new WebSocketLink({
-  uri:
-    process.env.NODE_ENV === "production"
-      ? "wss://mast-ventures-backend.herokuapp.com/graphql"
-      : `ws://localhost:4000/graphql`,
-  options: {
-    reconnect: true,
-    connectionParams: {
-      "x-jwt": authTokenVar() || "",
-    },
-  },
-});
 
 const httpLink = createHttpLink({
   uri:
@@ -46,7 +32,6 @@ const splitLink = split(
       definition.operation === "subscription"
     );
   },
-  wsLink,
   authLink.concat(httpLink)
 );
 
